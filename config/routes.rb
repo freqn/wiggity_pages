7df6bf15c25a4ea20816495a617b55979a496156
c <freqnlabs@gmail.com>
2014-09-03 21:39:19 -0400

    Added index method to contacts controller & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  match 'contacts', {via: :get, to: 'contacts#index'}
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
