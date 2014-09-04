6577f3985f98fac886f4060b538c79b603dabd55
c <freqnlabs@gmail.com>
2014-09-04 07:23:51 -0400

    Adding CRUD operations

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,4 +5,32 @@ def index
   render 'contacts/index'
 end
 
+def new
+  @contact = Contact.new
+  render 'contacts/new'
+end
+
+def create
+  @contact = Contact.new name: params[:name],
+                            email: params[:email],
+                            phone: params[:phone]
+  if @contact.save
+    render 'contacts/success'
+  else
+    render 'contacts/new'
+  end
+end
+
+def edit
+  @contact = Contact.find(params[:id])
+  render 'contacts/edit'
+end
+
+def show
+  @contact = Contact.find(params[:id])
+  render 'contacts/show'
+end
+
+
+
 end
